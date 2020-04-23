0d34507da53694884ef96cc34ab3c6ebae09ad77
Fabiano Nascimento <fabianorodrigo@gmail.com>
2020-04-23 10:27:35 -0300

    Inclusão propriedades no objeto vazio de OrdemServicoFull e inclusão de typecast

--- src/client/components/ordemServico/ordemServicoNova.ts
+++ src/client/components/ordemServico/ordemServicoNova.ts
@@ -1,8 +1,12 @@
-export const OrdemServicoNova = {
+import {OrdemServicoFull} from '../../../models';
+
+export const OrdemServicoNova = ({
     idContrato: -1,
     emergencial: false,
     idTipoOrdemServicoContrato: -1,
     nomeRequisitante: '',
     nomeFiscalTecnico: '',
     itens: [],
-};
+    entregaveis: [],
+    etapas: [],
+} as unknown) as OrdemServicoFull;
