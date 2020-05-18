b59f659bde2697c9fedb418fd7a4002f638a6403
Fabiano Nascimento <fabianorodrigo@gmail.com>
2020-05-17 21:18:36 -0300

    Criação de função que permite exibição de dialog de confirmação com chamada por função semelhante ao "confirm" nativo dos browsers

--- src/client/components/lib/dialogConfirmacao.tsx
+++ src/client/components/lib/dialogConfirmacao.tsx
@@ -5,14 +5,61 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import useStyles from '../../services/styles';
 import {HelpOutlineOutlinedIcon} from './icons';
 import {Transicao} from './Transicao';
+
+export function MostrarDialog(mensagem: string, detalhesMensagem?: string[]) {
+    const wrapper = document.body.appendChild(document.createElement('div'));
+
+    const promise = new Promise((resolve) => {
+        try {
+            ReactDOM.render(
+                <DialogConfirmacao
+                    mensagem={detalhesMensagem ? mensagem : 'Confirmação'}
+                    detalhesMensagem={detalhesMensagem ? detalhesMensagem : [mensagem]}
+                    funcaoFecharCallback={(sim: boolean) => {
+                        resolve(sim);
+                    }}
+                />,
+                wrapper,
+            );
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    });
+
+    function dispose() {
+        setTimeout(() => {
+            ReactDOM.unmountComponentAtNode(wrapper);
+            setTimeout(() => {
+                if (document.body.contains(wrapper)) {
+                    document.body.removeChild(wrapper);
+                }
+            });
+        }, 200);
+    }
+
+    return promise.then(
+        (result) => {
+            dispose();
+            return result;
+        },
+        (result) => {
+            dispose();
+            return Promise.reject(result);
+        },
+    );
+}
 
 export const DialogConfirmacao: React.FC<{
     mensagem: string;
     detalhesMensagem?: string[];
     funcaoFecharCallback: Function;
 }> = (props) => {
+    const classes = useStyles();
     const {mensagem, detalhesMensagem, funcaoFecharCallback} = props;
     const fechar = (sim: boolean) => {
         //setOpen(false);
@@ -27,7 +74,7 @@ export const DialogConfirmacao: React.FC<{
             aria-describedby="alert-dialog-description"
             TransitionComponent={Transicao}
         >
-            <DialogTitle id="alert-dialog-title">
+            <DialogTitle id="alert-dialog-title" className={classes.dialogTitle}>
                 <HelpOutlineOutlinedIcon fontSize="large" style={{float: 'left'}} />
                 {mensagem}
             </DialogTitle>
@@ -42,7 +89,7 @@ export const DialogConfirmacao: React.FC<{
                                         <b>{msgSplit[0]}:</b> {msgSplit[1]}
                                     </React.Fragment>
                                 ) : (
-                                    {msg}
+                                    <span>{msg}</span>
                                 )}
                                 <br key={`BRmsgDetalhe${i}`} />
                             </React.Fragment>
