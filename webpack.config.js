077ee5fe0f76217a546461279e646a5af9c4ef50
Fabiano Nascimento <fabianorodrigo@gmail.com>
2020-06-12 16:41:53 -0300

    - Utilização do plugin webpack-visualizer-plugin para gerar dados sobre tamanho do bundle.js - Configuração do webpack.config.js para tirar biblioteca material-ui do bundle.js (comentado para continuação futura)

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 const path = require('path');
+const visualizer = require('webpack-visualizer-plugin');
 
 module.exports = {
     mode: 'production',
-    context: path.join(__dirname, 'src/client'),
-    entry: ['./index.tsx'],
+    entry: ['./src/client/index.tsx'],
     output: {
         path: __dirname + '/public/js/',
         filename: 'bundle.js',
+        chunkFilename: '[name].chunk.js',
     },
     // Enable sourcemaps for debugging webpack's output.
-    devtool: 'source-map',
+    //devtool: 'source-map',
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: ['.ts', '.tsx', '.js'],
     },
+    plugins: [new visualizer()],
     module: {
         rules: [
             {
                 test: /\.ts(x?)$/,
-                exclude: /(node_modules|repositories|datasources|controllers)/,
+                include: [path.resolve(__dirname, 'src/client'), path.resolve(__dirname, 'src/commonLib')],
+                exclude: /(node_modules|repositories|datasources|controllers|dist|__tests__)/,
                 use: [
                     {
                         loader: 'ts-loader',
@@ -26,19 +29,37 @@ module.exports = {
                 ],
             },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
+            /*{
                 enforce: 'pre',
                 test: /\.js$/,
                 loader: 'source-map-loader',
-            },
+            },*/
         ],
     },
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-    },
+    target: 'web',
+    externals: [
+        {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+        },
+        //externalForMaterialUi,
+    ],
 };
+
+//TODO: A inclusão da função abaixo foi suficiente para remover as bibliotecas da material-ui do bundle
+// mas tem que copiar todos os JS's dentro de node_modules/@material-ui/***  e referenciá-los no HTML
+// Pensar depois como que utilizamos a importação dinâmica pois fica automatizado:  https://mariusschulz.com/blog/dynamic-import-expressions-in-typescript
+// Enquanto isso, nosso bundle fica com quase 1 MB
+/*
+function externalForMaterialUi(context, request, callback) {
+    if (/@material-ui.+/.test(request)) {
+        const name = request.replace(/^.*[\\\/]/, '');
+        return callback(null, 'root MaterialUI.' + name);
+    }
+    callback();
+}
+*/
